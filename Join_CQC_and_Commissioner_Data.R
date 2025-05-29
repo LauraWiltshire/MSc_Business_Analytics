a65fdb134949639a72284667c061081698129273
LauraWiltshire <119063277+LauraWiltshire@users.noreply.github.com>
2025-05-29 09:02:22 +0100

    Update and rename JoinCQCandCommissionerData.R to Join_CQC_and_Commissioner_Data.R

--- Join_CQC_and_Commissioner_Data.R
+++ Join_CQC_and_Commissioner_Data.R
@@ -1,8 +1,13 @@
-getwd()
+
+# Set Commissioner data
 setwd("C:/Users/wiltshl/OneDrive - BANES Council/Documents/MSc/Dissertation/Commissioner Quality and Risk Dashboards")
 commissioner_data <- read_csv('tidy_commissioner_data.csv')
+
+# Set CQC data
 setwd("C:/Users/wiltshl/OneDrive - BANES Council/Documents/MSc/Dissertation/Historic CQC Ratings")
 cqc_data <- read_csv('combined_ratings_data.csv')
+
+# Check all datasets
 glimpse(commissioner_data)
 glimpse(cqc_data)
 
@@ -13,18 +18,16 @@ library(dplyr)
 # Load the dplyr package
 library(dplyr)
 
-# Perform an inner join based on the specified columns
 joined_data <- commissioner_data %>%
   inner_join(cqc_data, by = c("CQC Registration Number" = "Location ID"))
 
 # View the resulting dataset
 head(joined_data)
 
-
 # View the resulting data
 view(joined_data)
 setwd("C:/Users/wiltshl/OneDrive - BANES Council/Documents/MSc/Dissertation")
 
-# Save the tidy data to a new file if needed
+# Save the tidy data to a new file, write as CSV
 write.csv(joined_data, "Dataset_for_MSc.csv", row.names = FALSE)
 
